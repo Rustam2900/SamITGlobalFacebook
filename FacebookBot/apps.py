f3f19f6ed7dcdc60086764ac5ac2ac675a8aa2ec
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2024-11-02 18:44:22 +0500

    facebook message django integratsa

---
+++ FacebookBot/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FacebookbotConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'FacebookBot'
---
+++ FacebookBot/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class UserProfile(models.Model):
+    user_id = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    last_message = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
